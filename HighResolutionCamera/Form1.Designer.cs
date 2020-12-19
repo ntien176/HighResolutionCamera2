d429a43180b107e18bf2ebc610ec228301adb0a3
Tien Nguyen <ntien176@gmail.com>
2020-12-19 15:36:57 +0700

    Add project files.

---
+++ HighResolutionCamera/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace HighResolutionCamera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start_btn = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.cboCamera = new System.Windows.Forms.ComboBox();
+            this.Take_btn = new System.Windows.Forms.Button();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(592, 454);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(75, 23);
+            this.start_btn.TabIndex = 1;
+            this.start_btn.Text = "&Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(184, 76);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(640, 360);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            // 
+            // cboCamera
+            // 
+            this.cboCamera.FormattingEnabled = true;
+            this.cboCamera.Location = new System.Drawing.Point(265, 49);
+            this.cboCamera.Name = "cboCamera";
+            this.cboCamera.Size = new System.Drawing.Size(199, 21);
+            this.cboCamera.TabIndex = 3;
+            // 
+            // Take_btn
+            // 
+            this.Take_btn.Location = new System.Drawing.Point(684, 455);
+            this.Take_btn.Name = "Take_btn";
+            this.Take_btn.Size = new System.Drawing.Size(67, 22);
+            this.Take_btn.TabIndex = 4;
+            this.Take_btn.Text = "Take";
+            this.Take_btn.UseVisualStyleBackColor = true;
+            this.Take_btn.Click += new System.EventHandler(this.Take_btn_Click);
+            // 
+            // save_btn
+            // 
+            this.save_btn.Location = new System.Drawing.Point(762, 454);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(79, 23);
+            this.save_btn.TabIndex = 5;
+            this.save_btn.Text = "Save";
+            this.save_btn.UseVisualStyleBackColor = true;
+            this.save_btn.Click += new System.EventHandler(this.save_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(183, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Camera:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 599);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.save_btn);
+            this.Controls.Add(this.Take_btn);
+            this.Controls.Add(this.cboCamera);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.start_btn);
+            this.Name = "Form1";
+            this.Text = "Webcame";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.ComboBox cboCamera;
+        private System.Windows.Forms.Button Take_btn;
+        private System.Windows.Forms.Button save_btn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HighResolutionCamera/Form1.cs
@@ -0,0 +1,92 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HighResolutionCamera
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Initialize camera library
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+                cboCamera.Items.Add(filterInfo.Name);
+            cboCamera.SelectedIndex = 1;
+            videoCaptureDevice = new VideoCaptureDevice();
+        }
+        
+        //c# webcam capture picture
+        private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pic.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        //Choose camera and open it
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cboCamera.SelectedIndex].MonikerString);
+            videoCaptureDevice.VideoResolution = selectResolution(videoCaptureDevice);
+            videoCaptureDevice.NewFrame += FinalFrame_NewFrame;
+            videoCaptureDevice.Start();
+        }
+        //Close the camera when closing the form
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning == true)
+                videoCaptureDevice.Stop();
+        }
+        
+        //Take a photo
+        private void Take_btn_Click(object sender, EventArgs e)
+        {
+            videoCaptureDevice.Stop();
+            pic.Image = (Bitmap)pic.Image;
+        }
+
+        //Set resolution 1920 x 1080
+        private static VideoCapabilities selectResolution(VideoCaptureDevice device)
+        {
+            foreach (var cap in device.VideoCapabilities)
+            {
+                if (cap.FrameSize.Height == 1080)
+                    return cap;
+                if (cap.FrameSize.Width == 1920)
+                    return cap;
+            }
+            return device.VideoCapabilities.Last();
+        }
+
+        //Save the image
+        private void save_btn_Click(object sender, EventArgs e)
+        {
+            if (pic.Image != null)
+            {
+                Bitmap varBmp = new Bitmap(pic.Image);
+                Bitmap newBitmap = new Bitmap(varBmp);
+                varBmp.Save(@"C:\Users\TN\Pictures\Camera Roll\savemode.jpeg", ImageFormat.Jpeg);
+                varBmp.Dispose();
+                varBmp = null;
+            }
+            else
+            { MessageBox.Show("null exception"); }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
